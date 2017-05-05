cf156b4c039e2a2e0df797bf5af07df08f0440fb
mwhorse46 <needmefordev@gmail.com>
2017-05-05 14:43:01 +0530

    Added endpoints for getting users, updating user, deprovision user by user_id.

--- controllers/ResourcesController.js
+++ controllers/ResourcesController.js
@@ -14,7 +14,51 @@ var appDir = require('path').dirname(require.main.filename);
  * @param {object} res -  response object.
  */
 var createUser = function(req, res) {
-  
+
+};
+
+/**
+ * API to get list of Users
+ *
+ * @method getUsers.
+ * @param {object} req -  request object.
+ * @param {object} res -  response object.
+ */
+var getUsers = function(req, res) {
+
+};
+
+/**
+ * API to get user details by user_id
+ *
+ * @method getUser.
+ * @param {object} req -  request object.
+ * @param {object} res -  response object.
+ */
+var getUser = function(req, res) {
+
+};
+
+/**
+ * API to update user
+ *
+ * @method updateUser.
+ * @param {object} req -  request object.
+ * @param {object} res -  response object.
+ */
+var updateUser = function(req, res) {
+
+};
+
+/**
+ * API to deprovision or deactivate user
+ *
+ * @method deprovisionUser.
+ * @param {object} req -  request object.
+ * @param {object} res -  response object.
+ */
+var deprovisionUser = function(req, res) {
+
 };
 
 /**
@@ -23,5 +67,9 @@ var createUser = function(req, res) {
  *
  */
 module.exports = {
-    "createUser": createUser
+    "createUser": createUser,
+    "getUsers": getUsers,
+    "getUser": getUser,
+    "updateUser": updateUser,
+    "deprovisionUser": deprovisionUser
 }
--- controllers/index.js
+++ controllers/index.js
@@ -14,20 +14,66 @@ var express = require('express'),
  *     produces:
  *       - application/json
  *     parameters:
- *       - name: username
- *         description: Username to use for login.
- *         in: formData
- *         required: true
- *         type: string
- *       - name: password
- *         description: User's password.
- *         in: formData
- *         required: true
- *         type: string
  *     responses:
  *       200:
  *         description: createUser
  */
 router.post('/Users', ResourcesController.createUser);
+
+/**
+ * @swagger
+ * /scim/v2/Users:
+ *   get:
+ *     description: retrieve users
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *     responses:
+ *       200:
+ *         description: getUsers
+ */
+router.get('/Users', ResourcesController.getUsers);
+
+/**
+ * @swagger
+ * /scim/v2/Users/{user_id}:
+ *   get:
+ *     description: fetching of users by user id
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *     responses:
+ *       200:
+ *         description: fetching user details by id
+ */
+router.get('/Users/:user_id', ResourcesController.getUser);
+
+/**
+ * @swagger
+ * /scim/v2/Users/{user_id}:
+ *   put:
+ *     description: updating user by user id
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *     responses:
+ *       200:
+ *         description: updating user by user id
+ */
+router.put('/Users/:user_id', ResourcesController.updateUser);
+
+/**
+ * @swagger
+ * /scim/v2/Users/{user_id}:
+ *   patch:
+ *     description: Deprovisioning users by user_id
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *     responses:
+ *       200:
+ *         description: Deprovisioning users by user_id
+ */
+router.patch('/Users/:user_id', ResourcesController.deprovisionUser);
 
 module.exports = router;
